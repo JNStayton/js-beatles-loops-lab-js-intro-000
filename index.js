ccd50c252cf1c2954b661f3224c8b2a6cbf31f00
Jessica Stayton <jessica.stayton@gmail.com>
2021-04-02 21:46:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+    var newArray = [];
+        for (var i = 0; i < musicians.length; i++) {
+          newArray.push(musicians[i] + " plays " + instruments[i])
+          }
+    return newArray;
+}
+
+
+function johnLennonFacts () {
+    var facts = ["He was the last Beatle to learn to drive",
+      "He was never a vegetarian",
+      "He was a choir boy and boy scout",
+      "He hated the sound of his own voice"];
+    var factsLoud = [];
+    var i = 0
+        while (i < facts.length) {
+          factsLoud.push(facts[i] + "!!!")
+          i++;
+        }
+    return factsLoud;
+}
